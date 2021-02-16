fb8c6227d4b016dfb3a65482ef9b31f2cb9f7681
mdillehay <mdillehay@gmail.com>
2021-02-15 20:50:05 -0800

    added comments to singlepagepull.py

--- singlepagepull.py
+++ singlepagepull.py
@@ -8,7 +8,8 @@ id_list = []
 id_name = []
 error_list = []
 
-
+# tqdm is a lightweight very simple progress bar feature. 
+# this  FOR LOOP pulls 'TypeID' data from the ESI API and builds a list that can be iterated over.
 for i in tqdm(range(40)):
 	esi_request = ('https://esi.evetech.net/latest/universe/types/?datasource=tranquility&page=' + str(i+1))
 	response = requests.get(esi_request)
@@ -21,7 +22,8 @@ for i in tqdm(range(40)):
 
 #print(id_list)
 
-
+# this FOR LOOP pulls 'Item Name' data from ESI API using the list of 'TypeID's built in above LOOP. It then builds
+# another LIST of  (TypeID, ItemName) sets for every TypeID in the above LIST. 
 for item in tqdm(id_list):
     esi_request = ('https://esi.evetech.net/latest/universe/types/' + str(item) + '/?datasource=tranquility&language=en-us')
     response = requests.get(esi_request)
@@ -37,18 +39,23 @@ for item in tqdm(id_list):
     except Exception:
         pass
 
+
+# Add info to a SQLITE DB
 conn = sqlite3.connect("EveNames2.db")
 conn.execute("CREATE TABLE IF NOT EXISTS ItemNames(TypeID, ItemName)")
 conn.executemany("INSERT into ItemNames(TypeID, ItemName) values (?, ?)", id_name)
 conn.commit()
 
+
+# Checks the 'error_list' for logged errors, if found, it attempts to pull that info from ESI again using the
+# 'updateSingleItem' Method from 'single_item_pull.py'. Then removes that item from list.
 if not error_list:
     pass
 else: 
     for item in error_list:
         try:
             sip.updateSingleItemInfo(item)
-            error_list.pop()
+            error_list.pop(len(error_list) - (len(error_list)-1))
         except Exception:
             pass
 
